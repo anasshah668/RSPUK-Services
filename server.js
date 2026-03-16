e107bcfefe00c34be436dbeb9f2666fb5cb54b48
Muhammad Ans Rizvi <ans.rizvi@2bvision.com>
2026-03-16 23:26:04 +0500

    cors error

--- server.js
+++ server.js
@@ -18,11 +18,13 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
-// Middleware
-// app.use(cors({
-//   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-//   credentials: true
-// }));
+// Middleware - Allow CORS from anywhere
+app.use(cors({
+  origin: true, // Allow all origins
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
